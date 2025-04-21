b3376ac57156ad0d9b7f9edd0d98010cbc24eba9
Black66s <crowz.gac@gmail.com>
2025-04-21 21:22:04 +0700

    update

---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateOAuthLogin(user: any) {
+    const existingUser = await this.prisma.user.upsert({
+      where: { email: user.email },
+      update: { picture: user.picture, name: user.name },
+      create: {
+        email: user.email,
+        picture: user.picture,
+        name: user.name,
+      },
+    });
+
+    const payload = { sub: existingUser.id };
+    const token = this.jwtService.sign(payload);
+
+    return {
+      access_token: token,
+      user: {
+        email: existingUser.email,
+        picture: existingUser.picture,
+      },
+    };
+  }
+}
